0d251a81971ae0a821aaa3cf23c91807bc3961c7
Davie987 <oonlinegigz@gmail.com>
2022-06-13 15:05:29 +0300

    project initialization

--- main.py
+++ main.py
@@ -1,4 +1,4 @@
-from fastapi import FastAPI
+from fastapi import FastAPI, HTTPException, status
 from fastapi.params import Body
 from pydantic import BaseModel
 from random import randrange
@@ -16,6 +16,13 @@ class Post(BaseModel):
 my_posts = [{'title': 'title of post1', 'content': 'content of post1', 'id': 1}, {'title': 'favourite foods', 'content': 'I like pizza', 'id':2}]
 
 
+# functions
+def Find_post_by_Id(id):
+    for p in my_posts:
+        if p['id'] == id:
+            return p
+
+
 # TODO: API STRUCTURE
 
 # getting all the posts
@@ -27,7 +34,24 @@ def get_posts():
 # creating posts
 @app.post("/posts")
 def create_post(post:Post):
+    # convert the pyadantic model to a python dictionary
     post_dict = post.dict()
-    post_dict["id"] = randrange(0, 1000000)
+    # adding the id
+    post_dict["id"] = randrange(0, 100000000)
+    # appending it to my posts
     my_posts.append(post.dict)
+    # returning the created post
     return {'data': post_dict}
+
+
+# getting an individual post
+@app.get('/posts/{id}')
+def get_post(id : int):
+    # a function for findind by id
+    post = Find_post_by_Id(id)
+    # raise an exception if post not found
+    if not post:
+        raise HTTPException(status_code=status.HTTP_404_NOT_FOUND, detail=f"post with ID: ({id}) not found!")
+    # return post if found
+    return {"detail": post}
+
